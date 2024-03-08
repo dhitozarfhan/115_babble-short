035a33a5925855ed3ec0ec8bf6a4b7609d4176e8
dhitozarfhan <dhitozarfhan@gmail.com>
2024-03-08 12:05:43 +0700

    menambahkan variabel

--- 115_babble short/115_babble short.cpp
+++ 115_babble short/115_babble short.cpp
@@ -4,18 +4,6 @@ using namespace std;
 int a[20];
 int n;
 
-void input() {
-    while (true) {
-        cout << "Masukan banyaknya elemen pada array : ";
-        cin >> n;
-        if (n <= 20)
-            break;
-        else {
-            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
-
-        }
-    }
-   
 
         
 
